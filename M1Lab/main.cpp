88cb7f782344a958a630d3c288a9ee0a52976451
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2026-01-12 15:52:54 +0000

    First m1lab.

---
+++ M1Lab/main.cpp
@@ -0,0 +1,15 @@
+// CSC 134
+// M1LAB
+// Daley Ottersbach
+// 1/12/2026
+
+#include <iostream>
+#include <stdint.h>
+
+int main() {
+    const std::string NAME = "Lorem Ipsum";
+    const int32_t APPLES = 100;
+    const float PRICE_PER_APPLE = 0.25f;
+
+    std::cout << "Welcome to " << NAME << "'s apple orchard." << std::endl;
+}
