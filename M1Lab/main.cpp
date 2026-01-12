9eea6aff1aa47d04ef83c8ef3c8c9a984bc56e06
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2026-01-12 16:27:55 +0000

    Simple lab1 finish.

--- M1Lab/main.cpp
+++ M1Lab/main.cpp
@@ -4,13 +4,19 @@
 // 1/12/2026
 
 #include <iostream>
+#include <iomanip>
 #include <stdint.h>
 
+const std::string NAME = "Jane Smith";
+const int32_t APPLES = 100;
+const float PRICE_PER_APPLE = 0.25f;
+
 int main() {
-    const std::string NAME = "Lorem Ipsum";
-    const int32_t APPLES = 100;
-    const float PRICE_PER_APPLE = 0.25f;
+    float totalCost = APPLES * PRICE_PER_APPLE;
 
     std::cout << "Welcome to " << NAME << "'s apple orchard." << std::endl;
+    std::cout << "We have " << APPLES << " apples in stock." << std::endl;
+    std::cout << "Apples are currently $" << std::setprecision(2) << PRICE_PER_APPLE << " each." << std::endl;
+    std::cout << "If you want them all, that will be $" << std::setprecision(2) << totalCost << "." << std::endl;
     return 0;
 }
---
+++ M1T1/main.cpp
@@ -0,0 +1,11 @@
+// CSC 134
+// M1T1
+// Daley Ottersbach
+// 1/12/2026
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
