155a60e70f6797acb214eae99b5a8d31b885da05
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2026-01-14 16:01:55 +0000

    Made hello world way bigger.

--- M1T1/main.cpp
+++ M1T1/main.cpp
@@ -1,11 +1,23 @@
 // CSC 134
 // M1T1
 // Daley Ottersbach
-// 1/12/2026
+// 1/14/2026
 
 #include <iostream>
 
+const char TEXT[] = "Hello world!";
+
 int main(int argc, char** argv) {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << TEXT << std::endl;
+    std::cout << "This isn't a variable." << std::endl;
+    std::cout << "The line two lines above this one is a variable." << std::endl;
+    if (argc > 1) {
+        std::cout << "There are " << (argc - 1) << " arguments." << std::endl;
+        for (int i = 1; i < argc; ++i) {
+            std::cout << "Arg " << i << ": " << argv[i] << "." << std::endl;
+        }
+    } else {
+        std::cout << "No arguments provided." << std::endl;
+    }
     return 0;
 }
