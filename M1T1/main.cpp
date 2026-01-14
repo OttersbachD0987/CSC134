bd2f5d3c0cc717d0769b539b983d8a0c1ad7135a
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2026-01-14 16:06:38 +0000

    Minor asthetic refactors.

--- M1T1/main.cpp
+++ M1T1/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char** argv) {
     std::cout << TEXT << std::endl;
     std::cout << "This isn't a variable." << std::endl;
     std::cout << "The line two lines above this one is a variable." << std::endl;
+
     if (argc > 1) {
         std::cout << "There are " << (argc - 1) << " arguments." << std::endl;
         for (int i = 1; i < argc; ++i) {
@@ -19,9 +20,11 @@ int main(int argc, char** argv) {
     } else {
         std::cout << "No arguments provided." << std::endl;
     }
+
     std::string userInput;
     std::cout << "Input your name here: ";
     std::cin >> userInput;
     std::cout << "Your name is " << userInput << "." << std::endl;
+    
     return 0;
 }
