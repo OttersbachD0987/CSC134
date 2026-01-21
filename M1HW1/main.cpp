ee2e991ad3eaaefc4606d156736a55bf429ee03e
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2026-01-21 16:33:21 +0000

    Turns out I'm stupid. Forgot the naming schema of projects.

--- M1HW1/main.cpp
+++ M1HW1/main.cpp
@@ -1,20 +1,21 @@
 // CSC 134
 // M1HW1
 // Daley Ottersbach
-// 1/12/2026
+// 1/21/2026
 
 #include <iostream>
 #include <iomanip>
 #include <stdint.h>
 
-const std::string MERCHANT_NAME = "Jane Smith";
-const int32_t DATE = 100;
-const float GROSS = 0.25f;
+const std::string MOVIE_NAME = "Movie";
+const int32_t DATE = 1900;
+const float GROSS = 0.0f;
 
 int main(int argc, char** argv) {
-    std::cout << "The movie " << MERCHANT_NAME << " is ." << std::endl;
-    std::cout << MERCHANT_NAME << " came out in " << DATE << "." << std::endl;
-    std::cout << MERCHANT_NAME << " had a worldwide gross of $" << std::setprecision(2) << std::fixed << GROSS << "." << std::endl;
+    std::cout << std::setprecision(2) << std::fixed;
+    std::cout << "The movie is " << MOVIE_NAME << "." << std::endl;
+    std::cout << MOVIE_NAME << " came out in " << DATE << "." << std::endl;
+    std::cout << MOVIE_NAME << " had a worldwide gross of $" << GROSS << " million." << std::endl;
     std::cout << "In the movie the character _ said \"\"." << std::endl;
     return 0;
 }
