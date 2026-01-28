120e2a7f2606ed53af861a0fdbd0f3ac3a591002
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2026-01-28 16:49:48 +0000

    Basic Craps thing in progress.

---
+++ M3T2/main.cpp
@@ -0,0 +1,46 @@
+// CSC 134
+// M3T1
+// Daley Ottersbach
+// 1/28/2026
+
+#include <iostream>
+#include <iomanip>
+#include <random>
+
+int main(int argc, char** argv) {
+    std::random_device random = std::random_device();
+    std::mt19937 generator = std::mt19937(random());
+    std::uniform_int_distribution<uint32_t> dieSixDistrubtion = std::uniform_int_distribution<uint32_t>(1, 6);
+    
+    //dieSixDistrubtion(generator) + dieSixDistrubtion(generator);
+
+    bool win = false;
+    bool running = true;
+    int32_t roll = dieSixDistrubtion(generator) + dieSixDistrubtion(generator);
+    int32_t points = roll;
+    switch (roll) {
+        case 7:
+        case 11:
+            win = true;
+            goto postGame;
+    }
+    while (running) {
+        roll = dieSixDistrubtion(generator) + dieSixDistrubtion(generator);
+        if (roll == points) {
+            win = true;
+            running = false;
+            break;
+        } else if (roll == 7) {
+            running = false;
+            break;
+        }
+    }
+    postGame:
+    if (win) {
+        std::cout << "You have won." << std::endl;
+    } else {
+        std::cout << "You have lost." << std::endl;
+    }
+
+    return 0;
+}
