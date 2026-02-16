922fe04da4cd720c6740204084008300acffab5f
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2026-02-16 16:31:03 +0000

    Did some more simple stuff.

--- M3Lab1/main.cpp
+++ M3Lab1/main.cpp
@@ -9,8 +9,18 @@
 #include <cstdint>
 #include <random>
 #include <vector>
+#include <functional>
+#include <array>
 
 std::uniform_real_distribution<float> PERCENT_DISTRIBUTION = std::uniform_real_distribution<float>(0, 1);
+
+std::uniform_int_distribution<uint32_t> DIE_TWO_DISTRIBUTION    = std::uniform_int_distribution<uint32_t>(1, 2);
+std::uniform_int_distribution<uint32_t> DIE_FOUR_DISTRIBUTION   = std::uniform_int_distribution<uint32_t>(1, 4);
+std::uniform_int_distribution<uint32_t> DIE_SIX_DISTRIBUTION    = std::uniform_int_distribution<uint32_t>(1, 6);
+std::uniform_int_distribution<uint32_t> DIE_EIGHT_DISTRIBUTION  = std::uniform_int_distribution<uint32_t>(1, 8);
+std::uniform_int_distribution<uint32_t> DIE_TEN_DISTRIBUTION    = std::uniform_int_distribution<uint32_t>(1, 10);
+std::uniform_int_distribution<uint32_t> DIE_TWELVE_DISTRIBUTION = std::uniform_int_distribution<uint32_t>(1, 12);
+std::uniform_int_distribution<uint32_t> DIE_TWENTY_DISTRIBUTION = std::uniform_int_distribution<uint32_t>(1, 20);
 
 struct EntityTemplate {
 public:
@@ -51,41 +61,8 @@ public:
 
 struct ItemStack {
 public:
-    uint32_t itemID;
+    size_t itemID;
     uint32_t stackSize;
-};
-
-struct LootResult {
-public:
-    std::vector<ItemStack> results;
-
-    LootResult(void) {
-        this->results = std::vector<ItemStack>();
-    }
-};
-
-class ILootEntry {
-public:
-    virtual LootResult Evaluate(std::mt19937& a_generator) = 0;
-};
-
-class ChanceLootEntry : public ILootEntry {
-public:
-    float chance;
-    ItemStack item;
-
-    ChanceLootEntry(float a_chance, ItemStack a_item) {
-        this->chance = a_chance;
-        this->item = a_item;
-    }
-
-    LootResult Evaluate(std::mt19937& a_generator) {
-        LootResult result = LootResult();
-        if (PERCENT_DISTRIBUTION(a_generator) <= this->chance) {
-            result.results.emplace_back(this->item);
-        }
-        return result;
-    } 
 };
 
 struct PlayerData : public EntityData {
@@ -97,10 +74,8 @@ public:
 
 struct NPCData : public EntityData {
 public:
-    const ILootEntry* lootEntry;
+    NPCData(const EntityTemplate* const a_entityTemplate) : EntityData(a_entityTemplate) {
 
-    NPCData(const EntityTemplate* const a_entityTemplate, const ILootEntry* const a_lootEntry) : EntityData(a_entityTemplate) {
-        this->lootEntry = a_lootEntry;
     }
 };
 
@@ -108,7 +83,62 @@ struct RoomData {
 public:
     std::string roomName;
     std::string roomDescription;
-    
+    bool backlink;
+    uint32_t minExisting;
+    uint32_t maxExisting;
+    uint32_t minNew;
+    uint32_t maxNew;
+
+    RoomData(std::string_view a_name, std::string_view a_description, bool a_backlink, uint32_t a_minExisting, uint32_t a_maxExisting, uint32_t a_minNew, uint32_t a_maxNew) {
+        this->roomName = a_name;
+        this->roomDescription = a_description;
+        this->backlink = a_backlink;
+        this->minExisting = a_minExisting;
+        this->maxExisting = a_maxExisting;
+        this->minNew = a_minNew;
+        this->maxNew = a_maxNew;
+    }
+};
+
+struct RoomInstance {
+public:
+    size_t roomID;
+    size_t roomIndex;
+    size_t connections[];
+
+    RoomInstance(size_t a_roomID, size_t a_roomIndex) {
+        this->roomID = a_roomID;
+        this->roomIndex = a_roomIndex;
+    }
+};
+
+enum class Screen {
+    TITLE,
+    GAME
+};
+
+enum class Menu {
+    PAUSE,
+    NONE
+};
+
+struct GameState {
+public:
+    bool running = true;
+    Screen screen;
+    Menu menu;
+
+    uint32_t usedRooms;
+    std::vector<RoomInstance> rooms;
+    PlayerData player = nullptr;
+
+    GameState(void) {
+        this->screen = Screen::TITLE;
+        this->menu = Menu::NONE;
+        this->usedRooms = 0;
+        this->rooms = std::vector<RoomInstance>();
+        this->player = PlayerData(&EntityTemplate(100, 30));
+    }
 };
 
 int main(int argc, char** argv) {
@@ -117,13 +147,46 @@ int main(int argc, char** argv) {
     std::mt19937 generator = std::mt19937(random());
     #pragma endregion
 
-    const ILootEntry* LOOT_ENTRIES[] = {
-
-    };
+    GameState gameState = GameState();
 
     const EntityTemplate ENTITY_TEMPLATES[] = {
-
+        EntityTemplate(32, 0)
     };
+
+    const ItemData ITEM_DATA[] = {
+        ItemData("Bean", "A single bean, you may only have 0b11111111 beans.", 0b11111111)
+    };
+
+    const std::array<RoomData, 4> ROOM_DATA = {
+        RoomData("Simple", "", true, 0, 1, 1, 3),
+        RoomData("Loopback", "", true, 1, 3, 0, 2),
+        RoomData("Downfall", "", false, 0, 1, 2, 4),
+        RoomData("Lotus", "", true, 0, 0, 2, 6)
+    };
+
+    std::function<size_t(void)> GetRandomRoom = [generator, ROOM_DATA]() {
+        return PERCENT_DISTRIBUTION(generator) * ROOM_DATA.size();
+    };
+
+    uint32_t choice;
+    while (gameState.running) {
+        switch (gameState.screen) {
+            case Screen::TITLE:
+                std::cout << "------------------------------\n>>> Ars Timoris\n------------------------------\n1) New Game\n2) Quit\n------------------------------\n\nOption: " << std::endl;
+                std::cin >> choice;
+                switch (choice) {
+                    case 1:
+                        gameState.screen = Screen::GAME;
+                        break;
+                    case 2:
+                        gameState.running = false;
+                        break;
+                }
+                break;
+            case Screen::GAME:
+                break;
+        }
+    }
 
     return 0;
 }
--- M3T2/main.cpp
+++ M3T2/main.cpp
@@ -11,8 +11,9 @@ int main(int argc, char** argv) {
     std::random_device random = std::random_device();
     std::mt19937 generator = std::mt19937(random());
     std::uniform_int_distribution<uint32_t> dieSixDistrubtion = std::uniform_int_distribution<uint32_t>(1, 6);
-    
+
     //dieSixDistrubtion(generator) + dieSixDistrubtion(generator);
+    // 2 & 3 & 12 == LOSE
 
     std::cout << "CRAPS" << std::endl;
 
@@ -22,30 +23,33 @@ int main(int argc, char** argv) {
     int32_t points = roll;
     std::cout << "You rolled a " << roll << "." << std::endl;
     switch (roll) {
+        case 2:
+        case 3:
+        case 12:
+            running = false;
+            break;
         case 7:
         case 11:
             win = true;
-            goto postGame;
+            break;
     }
-    std::cout << "Your target points are " << points << std::endl;
-    while (running) {
+    if (running != win) {
+        std::cout << "Your target points are " << points << std::endl;
+    }
+    while (running != win) {
         roll = dieSixDistrubtion(generator) + dieSixDistrubtion(generator);
         std::cout << "You rolled a " << roll << ".";
         std::cin.get();
         if (roll == points) {
             win = true;
-            running = false;
-            break;
         } else if (roll == 7) {
             running = false;
-            break;
         }
     }
-    postGame:
     if (win) {
         std::cout << "You won." << std::endl;
     } else {
-        std::cout << "You rolled a 7, that's a loss." << std::endl;
+        std::cout << "You rolled a " << roll << ", that's a loss." << std::endl;
     }
 
     return 0;
