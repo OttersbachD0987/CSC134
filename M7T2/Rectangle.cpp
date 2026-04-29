1ccc0e1733919f3f8d136a7f42a62a1f8abc9b68
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2026-04-29 15:12:26 +0000

    More work on m7t2.

---
+++ M7T2/Rectangle.cpp
@@ -0,0 +1,7 @@
+#include <cstdint>
+
+struct Rectangle {
+public:
+    uint32_t width;
+    uint32_t height;
+};
---
+++ M7T2/main.cpp
@@ -0,0 +1,32 @@
+// CSC 134
+// M7T1
+// Daley Ottersbach
+// 4/27/2026
+
+#include <iostream>
+#include <string>
+
+void Input(std::string& a_val) {
+    std::getline(std::cin, a_val);
+}
+
+int main(int argc, char** argv) {
+    std::string name = "Bob";
+    std::string* p_name = &name;
+    std::cout << "name    = " << name << std::endl;
+    std::cout << "p_name  = " << p_name << std::endl;
+    std::cout << "*p_name = " << *p_name << std::endl;
+
+    std::cout << "New Name: ";
+    Input(name);
+
+    std::cout << "name    = " << name << std::endl;
+    std::cout << "p_name  = " << p_name << std::endl;
+    std::cout << "*p_name = " << *p_name << std::endl;
+
+    *p_name = "Ana";
+
+    std::cout << "name    = " << name << std::endl;
+    std::cout << "p_name  = " << p_name << std::endl;
+    std::cout << "*p_name = " << *p_name << std::endl;
+}
